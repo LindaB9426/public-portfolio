a93adb16d6df03160e7b594045bb6a9eee5dd02c
LindaB9426 <babre.linda@inbox.lv>
2026-04-29 19:38:43 +0300

    counter

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Flex } from "@chakra-ui/react";
 
 const Footer = () => {
   const [showCounter, setShowCounter] = useState(true);
 
   useEffect(() => {
-
     if (window.location.hostname === "localhost") {
       setShowCounter(false);
     }
   }, []);
+
   return (
     <Box bgGradient="linear(to-b, #090909, #353535, #858585)">
       <footer>
@@ -20,15 +20,18 @@ const Footer = () => {
           justifyContent="center"
           alignItems="center"
           maxWidth="1024px"
-          height={20} 
-          flexDirection="column" 
+          height={20}
+          flexDirection="column"
         >
           <p>Linda • © 2026</p>
-          <img
-            src="https://komarev.com/ghpvc/?username=LindaB9426&label=Page%20views&color=0e75b6&style=flat"
-            alt="Page views"
-            style={{ marginTop: "4px" }}
-          />
+
+          {showCounter && (
+            <img
+              src="https://komarev.com/ghpvc/?username=LindaB9426&label=Page%20views&color=0e75b6&style=flat"
+              alt="Page views"
+              style={{ marginTop: "4px" }}
+            />
+          )}
         </Flex>
       </footer>
     </Box>
